f84ce574e79e07040aefbc203c3c3dae035c59b4
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 05:50:41 +0100

    modify like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update_likes_counter(post)
-    Post.find(post.id).increment!(:likes_counter)
+  after_create :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
