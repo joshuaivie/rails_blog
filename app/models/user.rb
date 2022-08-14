9fec360ad5e3a68b14cdbcc52c05eece55e18d07
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 05:50:59 +0100

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: { allow_blank: false, message: 'Please supply a user name' }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :posts
   has_many :comments
   has_many :likes
-
-  validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     Post.where(author_id: id).limit(3).order(updated_at: :asc)
