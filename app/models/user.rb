f546ccfa7bca4b64c5b6255d2394a909ef25ca2b
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 02:31:30 +0100

    create most_recent_posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def self.most_recent_posts(user)
+    Post.where(author_id: user.id).limit(3).order(updated_at: :asc)
+  end
 end
