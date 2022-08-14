22d191099b1a7fc64615a303dd1df345a7233366
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 05:50:50 +0100

    modify post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,20 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: { allow_blank: false, message: 'Please supply a post title' },
+                    length: { maximum: 250, message: 'Post title must not exceed 250 characters' }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.update_post_counter(user)
-    User.find(user.id).increment!(:posts_counter)
-  end
+  after_create :update_posts_counter
 
   def most_recent_comments
     Comment.where(post_id: id).limit(5).order(updated_at: :asc)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
