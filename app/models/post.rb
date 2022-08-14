bcda96db67a4cdf5665a081ef719f5972c21a668
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 02:37:12 +0100

    add update_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
+
+  def self.update_post_counter(user)
+    User.find(user.id).increment!(:posts_counter)
+  end
 end
