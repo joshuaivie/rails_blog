c648924c0f57da1fd504f0b71c5d8f9c84e899b4
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 02:55:02 +0100

    add update_likes_counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def self.update_comments_counter(post)
-    Post.find(post.id).increment!(comments_counter)
+    Post.find(post.id).increment!(:comments_counter)
   end
 end
