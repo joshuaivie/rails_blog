5133c7c2d61d1c1ac8afdfe64c0329e53335238f
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-14 02:40:38 +0100

    add most_recent_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def self.update_post_counter(user)
     User.find(user.id).increment!(:posts_counter)
   end
+
+  def self.most_recent_comments(post)
+    Comment.where(post_id: post.id).limit(5).order(updated_at: :asc)
+  end
 end
