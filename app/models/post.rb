2517b5eea8282e017dfaa2390e466cace703b393
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 00:41:00 +0100

    modify models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: '::User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-  belongs_to :author, class_name: 'User'
 
   validates :title, presence: { allow_blank: false, message: 'Please supply a post title' },
                     length: { maximum: 250, message: 'Post title must not exceed 250 characters' }
