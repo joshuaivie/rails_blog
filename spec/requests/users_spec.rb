ea685b1458d8401f34067c946f52a8055d5a3c96
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 01:41:24 +0100

    add spec for users#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template('index')
     end
   end
+
+  describe 'GET /users/1' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'has a successfull response status.' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template.' do
+      expect(response).to render_template('show')
+    end
+  end
 end
