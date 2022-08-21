da95d218ef29c198955f63340dd3a4402e66fc5a
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 01:57:59 +0100

    modify controller specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe 'Posts', type: :request do
     it 'renders the correct template.' do
       expect(response).to render_template('index')
     end
+
+    it 'includes the correct placeholder text.' do
+      expect(response.body).to include('This is a list of posts')
+    end
   end
 
   describe 'GET /users/1/posts/1' do
@@ -26,6 +30,10 @@ RSpec.describe 'Posts', type: :request do
 
     it 'renders the correct template.' do
       expect(response).to render_template('show')
+    end
+
+    it 'includes the correct placeholder text.' do
+      expect(response.body).to include('This is a single post')
     end
   end
 end
