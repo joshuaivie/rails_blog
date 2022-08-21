e45809b98b2eb7193c1ce308548859bc9f7ec17e
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 01:40:50 +0100

    add spec for users#index

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Comment, type: :model do
   context 'after create' do
     it 'Should update the post\'s comments_counter' do
       comment = Comment.create(post: Post.first, author: User.first, text: 'comment')
-
       expect(comment.post.comments_counter).to eq(comment.post.comments.length)
     end
   end
