c3264a67220897b386688a426f04c742f6fe030d
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 07:15:14 +0100

    fix errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,17 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.author = current_user
     @comment.post_id = params[:post_id].to_i
-    @comment.save
-    redirect_to user_post_path(params[:user_id], params[:post_id])
+
+    if @comment.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      flash[:error] = 'Error saving comment'
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
+    params.permit(:text)
   end
 end
