d9b13bcbe8e9c177eed5acc3b608e20cf8413942
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 04:47:20 +0100

    add pagination to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
+    @current_page = params['page'].nil? || params['page'].to_i < 1 ? 1 : params['page'].to_i
     @user = User.find(params['user_id'].to_i)
-    @posts = Post.where(author: @user).order(id: :desc).limit(3)
+    @posts = Post.where(author: @user).order(id: :asc).limit(3).offset((@current_page - 1) * 3)
+    @no_of_pages = (@user.posts_counter.to_f / 3).ceil
   end
 
   def show
