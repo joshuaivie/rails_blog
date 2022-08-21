5962413f60b5bfa7e47e7f958a82b165085fb464
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 10:55:32 +0100

    update likes posts
    Signed-off-by: Joshua O. Ivie <joshua@lifeonline.com.ng>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,36 @@
 class PostsController < ApplicationController
   def index
-    @current_page = params['page'].nil? || params['page'].to_i < 1 ? 1 : params['page'].to_i
-    @user = User.find(params['user_id'].to_i)
-    @posts = Post.where(author: @user).order(id: :asc).limit(3).offset((@current_page - 1) * 3)
-    @no_of_pages = (@user.posts_counter.to_f / 3).ceil
     @current_user = current_user
+    @user = User.find(params[:user_id].to_i)
+    @no_of_pages = (@user.posts_counter.to_f / 3).ceil
+    @current_page = params[:page].nil? || params[:page].to_i < 1 ? 1 : params[:page].to_i
+    @posts = Post.where(author: @user).order(id: :asc).limit(3).offset((@current_page - 1) * 3)
   end
 
   def new
-    @post = Post.new
-    @user = User.find(params['user_id'].to_i)
     @current_user = current_user
+    @post = Post.new
+    @user = User.find(params[:user_id].to_i)
   end
 
   def create
+    @current_user = current_user
     @post = Post.new(post_params)
     @post.author = current_user
     @post.comments_counter = 0
     @post.likes_counter = 0
     @user = current_user
-    @current_user = current_user
 
     if @post.save
-      redirect_to user_post_path(current_user, @post)
+      redirect_to user_post_path(@user, @post)
     else
-      render :new, status: :unprocessable_entity
+      flash[:error] = 'Error creating post'
     end
   end
 
   def show
-    @post = Post.find(params['id'].to_i)
     @current_user = current_user
+    @post = Post.find(params[:id].to_i)
   end
 
   private
