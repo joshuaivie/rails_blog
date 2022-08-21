c99b16f9b06ea9760524b52f98866cbd06f05902
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 01:43:43 +0100

    add spec for posts#index

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/1/posts' do
+    before(:example) do
+      get '/users/1/posts'
+    end
+
+    it 'has a successfull response status.' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template.' do
+      expect(response).to render_template('index')
+    end
   end
 end
