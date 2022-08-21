248dd246d4eb57f41b0547030327f6ce16cf3640
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-21 10:50:32 +0100

    update likes controller
    Signed-off-by: Joshua O. Ivie <joshua@lifeonline.com.ng>

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,13 @@
 class LikesController < ApplicationController
   def create
-    Like.create(author: current_user, post_id: params['post_id'].to_i)
-    redirect_to user_post_path(params['user_id'], params['post_id'])
+    @like = Like.new
+    @like.author = current_user
+    @like.post_id = params[:post_id].to_i
+
+    if @like.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      flash[:error] = 'Error saving comment'
+    end
   end
 end
