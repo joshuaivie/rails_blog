8829720a027a8a94b2aba840ec7435fe7cadc718
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-27 15:43:02 +0100

    add spec tests for user show page

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -57,4 +57,40 @@ RSpec.describe 'Users', type: :request do
       end
     end
   end
+
+  describe 'User show page' do
+    before(:example) { visit user_path(1) }
+    before(:all) { @user = User.find 1 }
+    it "I can see the user's profile picture." do
+      expect(page).to have_xpath("//img[@src='#{@user.photo}']")
+    end
+    it "I can see the user's username." do
+      expect(page).to have_content(@user.name)
+    end
+    it 'I can see the number of posts the user has written.' do
+      expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+    end
+    it "I can see the user's bio." do
+      expect(page).to have_content(@user.bio)
+    end
+    it "I can see the user's first 3 posts." do
+      @user.posts.order(created_at: :asc).limit(3).each do |post|
+        expect(page).to have_content(post.title)
+      end
+    end
+    it "I can see a button that lets me view all of a user's posts." do
+      expect(page).to have_content('All Posts')
+    end
+    it "When I click a user's post, it redirects me to that post's show page." do
+      @user.posts.order(created_at: :asc).limit(3).each do |post|
+        find(:xpath, "//a[@href='/users/#{@user.id}/posts/#{post.id}']").click
+        expect(page).to have_current_path("/users/#{@user.id}/posts/#{post.id}")
+        visit user_path(@user)
+      end
+    end
+    it "When I click on all posts, it redirects me to the user's post's index page." do
+      click_link('All Posts')
+      expect(page).to have_current_path("/users/#{@user.id}/posts")
+    end
+  end
 end
